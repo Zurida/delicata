e07fe5655d0ed3205ae4141adcd34e587567aa96
Zurida <undinka08@mail.ru>
2024-08-18 14:50:24 +0300

    VSelect css update

--- components/common/VSelect.vue
+++ components/common/VSelect.vue
@@ -25,7 +25,6 @@ function handleClick(option: TOption) {
 }
 
 
-
 useClickOutside(
     componentRef,
     () => {
@@ -37,11 +36,13 @@ useClickOutside(
 </script>
 
 <template>
-    <div class="custom-select" ref="custom-select" :class="{ active: isActive }">
+    <div class="VSelect" :class="{ active: isActive }">
         <button ref="excludeRef" class="select-button" role="combobox" aria-label="select button"
-            aria-haspopup="listbox" :aria-expanded="isActive" aria-controls="select-dropdown" @click="isActive = true">
+            aria-haspopup="listbox" :aria-expanded="isActive" aria-controls="select-dropdown"
+            @click="isActive = !isActive">
             <span class="selected-value">{{ selectedOption || 'Выбрать категорию' }}</span>
-            <span class="arrow"></span>
+            <!-- <span class="arrow"></span> -->
+            <i class="arrow right"></i>
         </button>
 
         <ul class="select-dropdown" role="listbox" id="select-dropdown" ref="componentRef">
@@ -54,116 +55,106 @@ useClickOutside(
     </div>
 </template>
 
-<style>
-.custom-select {
+<style lang="scss">
+.VSelect {
     position: relative;
-    width: 400px;
     max-width: 100%;
     font-size: 1.15rem;
     color: #000;
-    margin-top: 3rem;
-}
 
-.custom-select .select-button {
-    width: 100%;
-    font-size: 1.15rem;
-    background-color: #fff;
-    padding: 0.675em 1em;
-    border: 1px solid #caced1;
-    border-radius: 0.25rem;
-    cursor: pointer;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
+    .select-button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        font-size: inherit;
+        background-color: #fff;
+        padding: 0.675em 1em;
+        border-radius: 0.25rem;
+    }
 
-.custom-select .selected-value {
-    text-align: left;
-}
+    .arrow {
+        border: solid var(--black);
+        border-width: 0 2px 2px 0;
+        display: inline-block;
+        padding: 3px;
+        transform: rotate(45deg);
+        transition: transform .4s;
+    }
 
-.custom-select .arrow {
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-top: 6px solid #000;
-    transition: transform ease-in-out 0.3s;
-}
+    &.active {
+        .select-dropdown {
+            opacity: 1;
+            visibility: visible;
+            transform: scaleY(1);
+        }
 
-.custom-select.active .arrow {
-    transform: rotate(180deg);
-}
+        .arrow {
+            transform: rotate(-135deg);
+        }
+    }
 
-.custom-select.active .select-dropdown {
-    opacity: 1;
-    visibility: visible;
-    transform: scaleY(1);
-}
+    .select-dropdown {
+        position: absolute;
+        list-style: none;
+        width: 100%;
+        box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 10px;
+        margin-top: 10px;
+        max-height: 200px;
+        overflow-y: auto;
+        transition: 0.5s ease;
+        transform: scaleY(0);
+        opacity: 0;
+        visibility: hidden;
 
-.select-dropdown {
-    position: absolute;
-    list-style: none;
-    width: 100%;
-    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-    box-sizing: border-box;
-    background-color: #fff;
-    border: 1px solid #caced1;
-    border-radius: 4px;
-    padding: 10px;
-    margin-top: 10px;
-    max-height: 200px;
-    overflow-y: auto;
-    transition: 0.5s ease;
-    transform: scaleY(0);
-    opacity: 0;
-    visibility: hidden;
-}
+        &:focus-within {
+            box-shadow: 0 10px 25px rgba(94, 108, 233, 0.6);
+        }
 
-.select-dropdown:focus-within {
-    box-shadow: 0 10px 25px rgba(94, 108, 233, 0.6);
-}
+        label {
+            display: flex;
+            gap: 1rem;
+            align-items: center;
+            width: 100%;
+            padding: 8px 10px;
+            cursor: pointer;
+            transition: background-color .4s;
 
-.select-dropdown li {
-    position: relative;
-    cursor: pointer;
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-}
+            &:hover {
+                background-color: #f2f2f2;
+            }
+        }
 
-.select-dropdown li label {
-    width: 100%;
-    padding: 8px 10px;
-    cursor: pointer;
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-}
+        &::-webkit-scrollbar {
+            width: 4px;
+        }
 
-.select-dropdown::-webkit-scrollbar {
-    width: 7px;
-}
+        &::-webkit-scrollbar-track {
+            background: #f1f1f1;
+            border-radius: 25px;
+        }
 
-.select-dropdown::-webkit-scrollbar-track {
-    background: #f1f1f1;
-    border-radius: 25px;
-}
+        &::-webkit-scrollbar-thumb {
+            background: #ccc;
+            border-radius: 25px;
+        }
 
-.select-dropdown::-webkit-scrollbar-thumb {
-    background: #ccc;
-    border-radius: 25px;
-}
+        input:checked~label {
+            background-color: #f2f2f2;
+        }
 
-.select-dropdown li:hover,
-.select-dropdown input:checked~label {
-    background-color: #f2f2f2;
-}
+        input:focus~label {
+            background-color: #dfdfdf;
+        }
 
-.select-dropdown input:focus~label {
-    background-color: #dfdfdf;
-}
-
-.select-dropdown input[type="radio"] {
-    position: absolute;
-    left: 0;
-    opacity: 0;
+        input[type="radio"] {
+            position: absolute;
+            left: 0;
+            opacity: 0;
+        }
+    }
 }
 </style>
