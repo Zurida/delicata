8d8f380a891d19e3718e64a24f884f67247b5b5c
Zurida <undinka08@mail.ru>
2024-10-26 14:10:02 +0300

    added package.json

---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  },
+  // imports: {
+  //   dirs: ['types/*.ts', 'store/*.ts', 'types/**/*.ts'],
+  // },
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Montserrat: true,
+        Nunito: true,
+      }
+    }],
+    'nuxt-icons',
+    '@pinia/nuxt'
+  ],
+
+  nitro: {
+    storage: {
+      users: {
+        driver: 'memory',
+        base: '.cache',
+      }
+    }
+  },
+
+  routeRules: {
+    '/api/**': {
+      cors: true,
+    },
+    '/api/user/bob': {
+      // prerender: true,
+      isr: true,
+    },
+    '/page': {
+      headers: {
+        'x-hackathon': 'true'
+      }
+    }
+  },
+  compatibilityDate: '2024-07-17',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern'
+        }
+      },
+    },
+  }
+})
