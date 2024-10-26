0135ca2fc31f1180aca72c8f380bfa64720e3948
Zurida <undinka08@mail.ru>
2024-10-26 14:35:43 +0300

    gitignore update

--- .nuxt/imports.d.ts
+++ .nuxt/imports.d.ts
@@ -30,4 +30,5 @@ export { injectHead, useHead, useSeoMeta, useHeadSafe, useServerHead, useServerS
 export { useClickOutside } from '../composables/useClickOutside';
 export { useCategoryStore } from '../stores/category';
 export { defineStore, acceptHMRUpdate, usePinia, storeToRefs } from '../node_modules/@pinia/nuxt/dist/runtime/composables';
+export { useNuxtDevTools } from '../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
 export { definePageMeta } from '../node_modules/nuxt/dist/pages/runtime/composables';
