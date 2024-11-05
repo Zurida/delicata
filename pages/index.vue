c3d8995f310fa1171fcdcaa5c0775533f13326bc
Zurida <undinka08@mail.ru>
2024-11-05 16:34:59 +0300

    small fix

--- pages/index.vue
+++ pages/index.vue
@@ -34,55 +34,6 @@
 
 <style scoped lang="scss">
 .home {
-  display: flex;
-  min-height: 100vh;
-}
-
-header {
-  margin-bottom: calc(var(--gap) / 2);
-}
-
-h1 {
-  font-size: 2.4rem;
-  font-weight: 600;
-  text-transform: uppercase;
-}
-
-.actions {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: var(--gap);
-
-  &__link {
-    font-size: 1.2rem;
-  }
-}
-
-.main {
-  flex: 1;
-  padding: var(--gap);
-  background-color: var(--white-soft);
-}
-
-.cards {
-  display: grid;
-  flex: 1;
-  gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 0.5fr));
-}
-
-
-.aside {
-  width: 22rem;
-
-  h2 {
-    padding: var(--gap);
-    border-bottom: 1px solid gray;
-    font-size: 2.4rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    color: var(--white-soft);
-  }
+  background-color: aqua;
 }
 </style>
