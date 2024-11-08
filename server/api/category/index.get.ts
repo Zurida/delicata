ec5828768d9cbbfc855ff26673ff898e077178c1
Zurida <undinka08@mail.ru>
2024-11-08 14:55:23 +0300

    removed all components, server, types etc

--- server/api/category/index.get.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from "~/prisma/db"
-
-export default defineEventHandler(async (event) => {
-  const categories = await prisma.category.findMany({})
-  return categories
-})
--- server/api/recipe/index.get.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "~/prisma/db"
-
-
-export default defineEventHandler(async () => {
-  const recipes = await prisma.recipe.findMany()
-  return recipes
-})
