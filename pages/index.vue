27e977e0cda73b43b2a30152ce9d6b74afbddd06
Zurida <undinka08@mail.ru>
2024-11-08 15:31:15 +0300

    removed script part from index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,3 @@
-<script setup>
-
-
-const menu = useCategoryStore()
-// const { data: recipesGet } = await useFetch('/api/recipe')
-
-let currentId = ref(0);
-const searchVal = ref('')
-// const cards = ref(recipesGet)
-
-
-async function setActiveId(category) {
-  currentId.value = category.id
-
-  try {
-    // const response = await $fetch(`/api/filterRecipes?category=${category.value}`)
-
-    // cards.value = response
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-</script>
-
 <template>
   <div class="home">
     <aside class="aside reverse">
